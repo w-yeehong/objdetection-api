30a446861c2f2ae1f2b600a87d419507128b0105
Yee Hong <e0418195@comp.nus.edu.sg>
2021-11-25 00:09:59 +0800

    Make resizing of images optional

--- app/main.py
+++ app/main.py
@@ -17,7 +17,10 @@ app.add_middleware(
 detector = ObjectDetector()
 
 @app.post("/predict", )
-async def predict(b64: str = Body(...), min_score: Optional[float] = Body(0.2)):
+async def predict(
+            b64: str = Body(...),
+            resize_image: Optional[bool] = Body(True),
+            min_score: Optional[float] = Body(0.2)):
     if min_score < 0 or min_score > 0.9:
         raise HTTPException(status_code=400,
                             detail="Minimum score should be between 0 and 0.9 inclusive.")
@@ -26,15 +29,18 @@ async def predict(b64: str = Body(...), min_score: Optional[float] = Body(0.2)):
     if img_bytes == "":
         raise HTTPException(status_code=400, detail="Payload is not base64-encoded.")
 
-    img = ImageHandler(img_bytes)
-    if not img.is_valid():
+    img_handler = ImageHandler(img_bytes)
+    if not img_handler.is_valid():
         raise HTTPException(status_code=400, detail="Payload is not a valid image.")
 
-    resized_img = img.resize()
+    if resize_image:
+        img = img_handler.resize()
+    else:
+        img = img_handler.img
 
-    result, inference_time = detector.run(Encoding.img_to_bytes(resized_img))
+    result, inference_time = detector.run(Encoding.img_to_bytes(img))
 
-    img_with_boxes = img.draw_boxes(result["detection_boxes"],
+    img_with_boxes = img_handler.draw_boxes(result["detection_boxes"],
                     result["detection_class_entities"],
                     result["detection_scores"],
                     min_score=min_score)
